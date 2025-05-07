92ef51e42c01b76be38a45b91f2cf083c43c3438
it <yeongbin0909@naver.com>
2025-05-07 20:09:30 +0900

    add columns

--- streamlit.py
+++ streamlit.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
-for i in range(100):
-    st.write("Hello **World!**")
+col1, col2 = st.columns([2, 3])
+
+with col1:
+    st.title("col1")
+with col2:
+    st.title("col2")
