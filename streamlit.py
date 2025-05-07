105daa427104e2c1d05819d2dd6b57ec72a0c4c4
it <yeongbin0909@naver.com>
2025-05-07 20:18:43 +0900

    텝 추가

--- streamlit.py
+++ streamlit.py
@@ -1,9 +1,16 @@
 import streamlit as st
 
 col1, col2 = st.columns([2, 3])
+tab1, tab2 = st.tabs(["Tab A", "Tab B"])
 
 with col1:
     st.title("col1")
+
+    with tab1:
+        st.write("tab1")
+    with tab2:
+        st.write("tab2")
+
 with col2:
     st.title("col2")
 
