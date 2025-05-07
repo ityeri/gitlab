2a84cb062332f612f4603f3dbd8995b075013183
it <yeongbin0909@naver.com>
2025-05-07 20:20:38 +0900

    코드 정리

--- streamlit.py
+++ streamlit.py
@@ -1,19 +1,22 @@
 import streamlit as st
 
 col1, col2 = st.columns([2, 3])
-tab1, tab2 = st.tabs(["Tab A", "Tab B"])
 
 with col1:
     st.title("col1")
 
-
 with col2:
     st.title("col2")
 
-    with tab1:
-        st.write("tab1")
-    with tab2:
-        st.write("tab2")
-
 col1.subheader("sub header")
 col2.checkbox("checkbox")
+
+
+
+tab1, tab2 = st.tabs(["Tab A", "Tab B"])
+
+with tab1:
+    st.write("tab1")
+
+with tab2:
+    st.write("tab2")
